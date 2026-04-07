8691c53b212e5c1a4bc667db917d20889a8c5be3
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2026-04-07 13:00:55 -0500

    Add handler for item

---
+++ internal/handler/item_handler.go
@@ -0,0 +1,53 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/crisywini/pinta-api/internal/model"
+	"github.com/crisywini/pinta-api/internal/service"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+)
+
+type ItemHandler struct {
+	service *service.ItemService
+}
+
+func NewItemHandler(service *service.ItemService) *ItemHandler {
+	return &ItemHandler{
+		service: service,
+	}
+}
+
+// Create a new item
+
+func (h *ItemHandler) PostItem(c *gin.Context) {
+	var item *model.Item
+
+	if err := c.ShouldBindBodyWith(&item, binding.JSON); err != nil {
+		response, serviceError := h.service.Create(item)
+
+		if serviceError != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": serviceError.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"id": response.ID.Hex(),
+		})
+	}
+}
+
+func (h *ItemHandler) GetAllItems(c *gin.Context) {
+
+	itemId := c.Query("id")
+
+	if len(itemId) == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Missing item id",
+		})
+	}
+
+}
