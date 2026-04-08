6325de3016cea59bb910b6586bbd278dbae83e64
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2026-04-08 12:07:04 -0500

    Add put and delete handlers for item

--- internal/handler/item_handler.go
+++ internal/handler/item_handler.go
@@ -72,3 +72,37 @@ func (h *ItemHandler) GetAllItems(c *gin.Context) {
 		"items": allItems,
 	})
 }
+
+func (h *ItemHandler) PutItem(c *gin.Context) {
+
+	id := c.Query("id")
+	var updatedItem *model.Item
+	if err := c.ShouldBindBodyWith(&updatedItem, binding.JSON); err == nil {
+		serviceError := h.service.Update(id, updatedItem)
+		if serviceError != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": "Error while updating item",
+				"error":   serviceError.Error(),
+			})
+		}
+	} else {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Malformed request",
+			"error":   err.Error(),
+		})
+	}
+}
+
+func (h *ItemHandler) DeleteItem(c *gin.Context) {
+	id := c.Query("id")
+
+	if err := h.service.DeleteById(id); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Error while deleting item",
+			"error":   err.Error(),
+		})
+	} else {
+		c.JSON(http.StatusNoContent, gin.H{})
+	}
+
+}
